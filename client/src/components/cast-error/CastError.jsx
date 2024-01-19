548ed2f57a0092fb795eacabec5447c24883599a
Velislav Lonev <lonevvelislav@gmail.com>
2024-01-19 11:45:21 +0200

    home button added to error page

--- client/src/components/cast-error/CastError.jsx
+++ client/src/components/cast-error/CastError.jsx
@@ -1,3 +1,16 @@
+import { Link } from "react-router-dom";
+
 export default function CastError() {
-  return <h1>Cast Error</h1>;
+    return (
+        <>
+            <h1 className="error-message ">Eror 404 &#9747;</h1>
+            <Link
+                className="btn btn-connect"
+                style={{ fontSize: "2rem" }}
+                to={"/"}
+            >
+                Home
+            </Link>
+        </>
+    );
 }
